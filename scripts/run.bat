e0fc62da0953e31de1125ff85891f56ed7fc7712
Manas <prometheansacrifice@gmail.com>
2019-09-23 01:01:59 +0530

    Bump npm -cli

--- scripts/run.bat
+++ scripts/run.bat
@@ -2,6 +2,6 @@ call esy build
 call esy npm-release
 call cd _release
 call npm pack
-call npm install -g ./pesy-0.5.0-alpha.2.tgz
+call npm install -g ./pesy-0.5.0-alpha.6.tgz
 call cd ..
 call .\_build\install\default\bin\TestPesyConfigure.exe
